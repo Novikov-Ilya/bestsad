ebdd2759dcef92be47868e22812ceb0d82538754
Ilya Novikov <mr.smile_lp@mail.ru>
2017-09-22 20:48:32 +0300

    start_add_advert.js was refactored according new php code

--- themes/thisis/js/start_add_advert.js
+++ themes/thisis/js/start_add_advert.js
@@ -1,27 +1,4 @@
 function redirect_adv() {
 
-
-var val_1 = document.getElementById('type_of_advert').value;
-
-switch (val_1) {
-	case "Аксесуары":
-	window.location.href = "/";
-	break;
-
-	case "Подгузники":
-	window.location.href = "/about";
-	break;
-
-	case "Игрушки":
-	window.location.href = "/";
-	break;
-
-	case "Коляски":
-	window.location.href = "/";
-	break;
-
-	case "Прочее":
-	window.location.href = "/";
-	break;
-	}
+window.location.href = document.getElementById('type_of_advert').value;
 }
