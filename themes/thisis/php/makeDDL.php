e9a4f701229f142e049539ea5946027eb8f21335
Ilya Novikov <mr.smile_lp@mail.ru>
2017-09-22 20:36:51 +0300

    sql request was changed. HTML select has new values (links)

--- themes/thisis/php/makeDDL.php
+++ themes/thisis/php/makeDDL.php
@@ -7,22 +7,23 @@ $conn = mysql_connect('localhost', 'root', '')
 or die('Не удалось соединиться: ' . mysql_error());
 mysql_select_db($db_name, $conn) or die('Не удалось выбрать базу данных');
 
-$query = mysql_query ("SELECT name FROM taxonomy_term_field_data WHERE vid = 'temy_obavlenii'");
+$query = mysql_query ("SELECT name, field_theme_advert_url_value FROM taxonomy_term_field_data, taxonomy_term__field_theme_advert_url WHERE entity_id = tid");
 
 $number = mysql_numrows ($query);
-echo $conn;
+
 $i = 0;
 
 
 if ($query) {
 	while ($i < $number) {
-		echo "<option value'";
-		echo mysql_result($query,$i,"name");
+		echo "<option value='";
+		echo mysql_result($query,$i,"field_theme_advert_url_value");
 		echo "'>";
 		echo mysql_result($query,$i,"name");
 		echo "</option>";
 		$i++;
 	}
 }
-else {echo "string";} 
+else {echo "Нет соединения с БД";} 
+mysql_close();
 ?>
